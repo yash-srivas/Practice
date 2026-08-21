93c4cd9319fa58d6bf24977b57bb889e316f6d4c
yash-srivas <itzmerohan47@gmail.com>
2026-08-22 03:09:44 +0530

    Time: 35 ms (8.43%) | Memory: 23.2 MB (8.58%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        multiset<int> m1(nums.begin(),nums.end());
+        set<int> temp(nums.begin(),nums.end());
+        vector<int> m2(temp.begin(),temp.end());
+        vector<pair<int,int>> m3;
+        for(int i=0;i<m2.size();i++){
+        int cnt=m1.count(m2[i]);
+        m3.push_back({cnt,m2[i]});
+        }
+        sort(m3.begin(),m3.end(),greater<pair<int,int>>());
+        vector<int> res;
+        for(int i=0;i<k;i++){
+            res.push_back(m3[i].second);
+        }
+        return res;
+    }
+};
